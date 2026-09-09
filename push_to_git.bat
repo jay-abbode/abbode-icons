68d5004c85cf65922b97e553fbf8e39d68771d0e
jay-abbode <jay@shopabbode.com>
2026-09-09 11:38:31 -0400

    Fix assets build: restore newSlugs scope + keep Include drafts checkbox

--- push_to_git.bat
+++ push_to_git.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "C:\Users\abbod\Dropbox\File Processing (Don't Open)\ICON APP\abbode-icons"
 git add -A
-git commit -m "Downloads: add Include drafts checkbox (active-only by default, archived never)"
+git commit -m "Fix assets build: restore newSlugs scope + keep Include drafts checkbox"
 git push
 pause
